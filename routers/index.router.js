106b54ecde49157910e209491535b3299e9f977a
Aaron Schwartz <aschwartz628@gmail.com>
2016-10-19 18:44:24 -0400

    refactored to index router from index.js

--- routers/index.router.js
+++ routers/index.router.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var index = express.Router();
+var path = require('path');
 
-index.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root:__dirname});
+router.get('/', function(request, response){
+  response.sendFile('./public/html/index.html', {root:path.resolve('./')});
 });
 
 module.exports = index;
