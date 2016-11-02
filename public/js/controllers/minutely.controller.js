e4afb190bda0bfd5a3f6caf7749ea610c59e730b
Aaron Schwartz <aschwartz628@gmail.com>
2016-11-02 18:39:05 -0400

    demonstrated ng repeat and usage of data contrained in a service including weather data, latitude, and longitude

--- public/js/controllers/minutely.controller.js
+++ public/js/controllers/minutely.controller.js
@@ -5,12 +5,15 @@
 MinutelyController.$inject = ['$scope', 'WeatherService'];
 
 function MinutelyController($scope, WeatherService){
-  $scope.createLocation = createLocation;
+  $scope.minuteLat = WeatherService.lat;
+  $scope.minuteLon = WeatherService.lon;
   $scope.weather = WeatherService.weatherData;
 
   $scope.$watch(function(){
     return WeatherService.weatherData;
   }, function(newVal, oldVal){
+    $scope.minuteLat = WeatherService.lat;
+    $scope.minuteLon = WeatherService.lon;
     $scope.weather = WeatherService.weatherData;
   });
   function createLocation(latitude, longitude){
--- public/js/services/weather.service.js
+++ public/js/services/weather.service.js
@@ -8,8 +8,7 @@ function WeatherService($http){
     var secretToken = {
       secret: 'its a secret'
     };
-    var lat;
-    var lon;
+
     var service = {};
     service.weatherData = [];
     service.createLocation = createLocation;
@@ -17,15 +16,14 @@ function WeatherService($http){
 
 
   function createLocation(latitude, longitude){
-    lat = latitude;
-    lon = longitude;
+    service.lat = latitude;
+    service.lon = longitude;
     var config = {
       headers: secretToken
     };
     var url = '/forecast/' + latitude + ',' + longitude;
       return $http.get(url, config)
                   .then(function(response){
-                    debugger;
                     service.weatherData = response.data;
                   });
   }
